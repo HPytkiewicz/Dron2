c13a0e77d04a684b6d4fe3a91d16bc51205fe8ec
Hubert <hubert@hubert.computer>
2020-05-25 15:21:36 +0200

    Dron1

--- inc/Dron.hh
+++ inc/Dron.hh
@@ -31,6 +31,9 @@ public:
 Dron()=delete;
 /*!
     * \brief Konstruktor przyjmujacy wierzcholki ciala drona, jego srodek oraz wskaznik na lacze do gnuplota
+    * \param nowe_wierzcholki - wierzcholki ciala drona
+    * \param nowy_srodek - wspolrzedne srodka drona
+    * \param gnuplot - wskaznik na lacze do gnuplota
     */
 Dron(vector<Wektor3D> nowe_wierzcholki, Wektor3D nowy_srodek, std::shared_ptr<drawNS::Draw3DAPI> gnuplot): Prostopadloscian(nowe_wierzcholki, 
 nowy_srodek, gnuplot), SrubaL({-3,-9.5,0}, gnuplot), SrubaP({3,-9.5,0}, gnuplot)
@@ -44,8 +47,17 @@ this->srodek={0,0,0};
 this->lacze=gnuplot;
 }
 
+/*!
+    * \brief Metoda animujaca obrot drona
+    */
 void animacjaObrotu();
+/*!
+    * \brief Metoda animujaca ruch drona
+    */
 void animacjaNaprzod();
+/*!
+    * \brief Metoda animujaca ruch srub
+    */
 void animacjaSruby();
 
 };
